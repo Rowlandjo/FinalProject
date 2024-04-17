bcb21844acd457b8d74f9c357ca9e37ef68cb69e
nataliep31 <166172560+nataliep31@users.noreply.github.com>
2024-04-17 11:50:58 -0400

    Update FinalProjectContext.cs

--- FinalProject/Data/FinalProjectContext.cs
+++ FinalProject/Data/FinalProjectContext.cs
@@ -18,11 +18,19 @@ namespace FinalProject.Data
         {
             builder.Entity<TeamMember_>().HasData(
                 new TeamMember { Id = 1, Fullname = "Natalie Plunkett", BirthDate = "January 31", CollegeProgram = "Information Technology", YearInProgram = "Senior" }
-               new TeamMember { Id = 2, Fullname = "Josh Rowland", BirthDate = "April 7", CollegeProgram = "Information Technology", YearInProgram = "Junior" }
-               
-
-
-
+                new TeamMember { Id = 2, Fullname = "Josh Rowland", BirthDate = "April 7", CollegeProgram = "Information Technology", YearInProgram = "Junior" }
+                );
+            builder.Entity<Hobby>().HasData(
+                new Hobby { Id = 1, HobbyName = "Gardening", TypeOfHobby = "Outdoors", CostOfHobby = "Low", TimeWhenHobbyPerformed = "Daytime" }
+                );
+            builder.Entity<CareerGoals>().HasData(
+                new Hobby { Id = 1, GoalName = "Technical Specialist", ResourcesReq = "Schooling, Certificates, Experience", SupportNeeded = "Yes", SuccessIndicator = "Getting a Job" }
+                );
+            builder.Entity<FavoriteFood>().HasData(
+                new Hobby { Id = 1, FoodName = "Pizza", Cuisine = "Italian", FlavorProfile = "Savory", PrepTime = 2, Healthy = "No" }
+                );
+        }                
+        
         public DbSet<FinalProject.Models.TeamMember_> TeamMember_ { get; set; } = default!;
         public DbSet<FinalProject.Models.Hobby> Hobby { get; set; } = default!;
         public DbSet<FinalProject.Models.CareerGoals> CareerGoals { get; set; } = default!;
