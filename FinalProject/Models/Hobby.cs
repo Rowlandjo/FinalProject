7a2f1da5fe5c811b1b09183a14de88a19dc80d5a
Rowlandjo <97526642+Rowlandjo@users.noreply.github.com>
2024-04-22 16:41:51 -0400

    Update FinalProjectContext.cs

--- FinalProject/Models/Hobby.cs
+++ FinalProject/Models/Hobby.cs
@@ -7,5 +7,15 @@
         public string TypeOfHobby { get; set; }
         public string CostOfHobby { get; set; }
         public int TimeWhenHobbyPerformex { get; set; }
+        public string FoodName { get; internal set; }
+        public string GoalName { get; internal set; }
+        public string Cuisine { get; internal set; }
+        public string FlavorProfile { get; internal set; }
+        public int PrepTime { get; internal set; }
+        public string Healthy { get; internal set; }
+        public string SupportNeeded { get; internal set; }
+        public string TimeWhenHobbyPerformed { get; internal set; }
+        public string ResourcesReq { get; internal set; }
+        public string SuccessIndicator { get; internal set; }
     }
 }
