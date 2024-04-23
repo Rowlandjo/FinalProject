cb6cc5e9987b9baa7905d6906820257bf6e305ba
Ashley Curran <curranay@mail.uc.edu>
2024-04-22 22:47:08 -0400

    Adjusted to required

--- FinalProject/Models/FavoriteFood.cs
+++ FinalProject/Models/FavoriteFood.cs
@@ -3,10 +3,10 @@
     public class FavoriteFood
     {
         public int Id { get; set; }
-        public string FoodName { get; set; }
-        public string Cuisine { get; set; }
-        public string FlavorProfile { get; set; }
+        public required string FoodName { get; set; }
+        public required string Cuisine { get; set; }
+        public required string FlavorProfile { get; set; }
         public int PrepTime { get; set; }
-        public string Healthy { get; set; }
+        public required string Healthy { get; set; }
     }
 }
