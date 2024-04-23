8d250ff7bf7acec7c47936e6ca6a11d6cb2962de
Ashley Curran <curranay@mail.uc.edu>
2024-04-22 22:47:28 -0400

    Adjuted to required

--- FinalProject/Models/CareerGoals.cs
+++ FinalProject/Models/CareerGoals.cs
@@ -3,9 +3,9 @@
     public class CareerGoals
     {
         public int Id { get; set; }
-        public string GoalName { get; set; }
-        public string ResourcesReq { get; set; }
-        public string SupportNeeded { get; set; }
-        public string SuccessIndicator { get; set; }
+        public required string GoalName { get; set; }
+        public required string ResourcesReq { get; set; }
+        public required string SupportNeeded { get; set; }
+        public required string SuccessIndicator { get; set; }
     }
 }
